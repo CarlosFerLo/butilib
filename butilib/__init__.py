fbd0788df01e08f28ae32d7f5e1445f3abdb9e4b
Carlos Fernandez <carlos.ferlo@outlook.com>
2023-12-18 11:46:58 +0100

    relocate descriptions to descriptions.py file

--- butilib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .suit import Suit, OROS, BASTOS, COPAS, ESPADAS
-from .card import Card, CardSet
-from .deck import Deck
-from .model import Model
-from .schema import CantarInput, CantarOutput, ContrarInput, CardSetDescription, SuitDescription
