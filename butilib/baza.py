397cc276c03eb96dbb14021ba7cef1592231a995
Carlos Fernandez <carlos.ferlo@outlook.com>
2024-01-01 18:26:33 +0100

    add documentation to baza.py

--- butilib/baza.py
+++ butilib/baza.py
@@ -4,6 +4,17 @@ from typing import List
 from .card import Card
 
 class Baza (BaseModel) :
+    """ A baza is a the object that represents a baza when playing butifarra. It is mainly included inside a Hystory object.
+
+    Attributes :
+        initial_player (int): an int representing the player that started the baza.
+        cards (List[Card]): ordered list of cards, in the order played
+        
+    Validations :
+        check_cards_has_no_repeated_elements: checks there are no repeated cards in a baza.
+    
+    """
+    
     initial_player: int = Field(ge=0, le=3)
     cards: List[Card] = Field(max_length=4)
  
@@ -15,9 +26,23 @@ class Baza (BaseModel) :
         return v
     
     def add (self, card: Card) -> None :
+        """Add a new card to the baza, it gets appended at the end of the cards attribute.
+
+        Args:
+            card (Card): Card to be added.
+        """
         self.cards.append(card)
         
     def __eq__(self, __value: object) -> bool:
+        """ Compare two cards for equality.
+
+        Args:
+            __value (object): Object to be compared with the card, expected to be a Card, otherwise return false.
+
+        Returns:
+            bool: Weather equality holds.
+        """
+        
         if self.initial_player != __value.initial_player :
             return False
         if self.cards != __value.cards :
@@ -25,6 +50,16 @@ class Baza (BaseModel) :
         
         return True
 class History (BaseModel) :
+    """ A set of bazas belonging to the same game, they are stored in an ordered way.
+
+        Attributes:
+            bazas (List[Baza]): Ordered list of all the bazas of a game (max 12).
+            
+        Validators:
+            validate_all_bazas_are_complete: make sure all bazas are complete.
+            validate_bazas_has_no_repeated_cards: make sure there are no repeated cards among all the bazas in History.
+            
+    """
     bazas: List[Baza] = Field(max_length=12)
     
     @field_validator("bazas")
@@ -48,16 +83,31 @@ class History (BaseModel) :
         return v
     
     def add (self, v: Baza | List[Baza]) -> None :
+        """ Add one or more bazas to the end of the bazas attribute.
+
+        Args:
+            v (Baza | List[Baza]): Baza or list of bazas to append.
+        """
         if isinstance(v, list) :
             self.bazas.extend(v)
         else : 
             self.bazas.append(v)
             
     def __iter__(self) :
+        """ Initialize History as an iterator over bazas attribute.
+        """
         self._i = 0
         return self
     
     def __next__(self) -> Baza:
+        """ Return the next element of the iteration over bazas.
+
+        Raises:
+            StopIteration: The iteration has finished, no more elements on bazas.
+
+        Returns:
+            Baza: The next baza in bazas.
+        """
         try :
             x = self.bazas[self._i]
             self._i += 1
@@ -66,7 +116,20 @@ class History (BaseModel) :
             raise StopIteration
         
     def __eq__(self, __value: object) -> bool:
+        """ Check for equality among two history instances.
+
+        Args:
+            __value (object): object to check equality against, expected to be of class History
+
+        Returns:
+            bool: Weather the vales are equal or not
+        """
         return self.bazas == __value.bazas
     
     def __len__(self) -> int :
+        """ Get the length of the bazas attribute.
+
+        Returns:
+            int: Number of bazas in History.
+        """
         return len(self.bazas)
