22f2bead1b9d6f7dba4fb7e8cf1c505fd97c75eb
Carlos Fernandez <carlos.ferlo@outlook.com>
2024-01-25 18:25:34 +0100

    add testing model

---
+++ butilib/testing/model.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from butilib.model import Model
+from butilib.card import Card
+from butilib.schema import PlayInput, PlayOutput
+
+class TestModel (Model) :
+    card_list: List[Card]
+    _i = 0
+    
+    def play(self, input: PlayInput) -> PlayOutput:
+        card = self.card_list[self._i]
+        self._i += 1 
+        self._i %= len(self.card_list)
+        
+        return PlayOutput(card=card)
