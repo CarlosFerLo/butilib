39a5667c4dfe958cf15b3c0ffb29e53004276ea4
Carlos Fernandez <carlos.ferlo@outlook.com>
2024-01-25 18:35:40 +0100

    add __test__=Fals flag to prevent pytest to try and run tests on it

--- butilib/testing/model.py
+++ butilib/testing/model.py
@@ -6,7 +6,9 @@ from butilib.schema import PlayInput, PlayOutput
 
 class TestModel (Model) :
     card_list: List[Card]
-    _i = 0
+    _i: int = 0
+    
+    __test__=False
     
     def _play(self, input: PlayInput) -> PlayOutput:
         card = self.card_list[self._i]
